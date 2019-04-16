b03ed46d7cb5d8f878cfa90c361b48ed84badc53
Marla Rogers <MarlaRogers@Marlas-MacBook-Pro.local>
2019-04-15 22:12:53 -0700

    still missing html

---
+++ server.js
@@ -0,0 +1,60 @@
+var mongoose = require('mongoose');
+var express = require('express');
+var cheerio = require('cheerio');
+var axios = require('axios');
+
+var app = express();
+var port = 8000
+var articles = []
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+const Article = mongoose.model('Article', { title: String, link: String });
+app.get("/", function(req, res) {
+    axios.get("https://news.ycombinator.com/")
+    .then(function(response) {
+        var html = response.data;
+        var $ = cheerio.load(html);
+        $(".title").each(function(i, element) {
+            var title = $(element).children("a").text();
+            var link = $(element).children("a").attr("href");
+            // console.log(title);
+            // console.log(author);
+            // console.log(readTime);
+            var article = {
+                title,
+                link,
+            }
+            if (title && link) {
+                const article = new Article({ title: title, link: link });
+                article.save().then(() => console.log('meow'));
+                // articles.push(article);
+            }
+            // console.log(articles);
+        })
+    })
+    
+
+/////arrow function is syntax and this
+
+
+})
+
+app.get("/api/articles", function(req, res) {
+    Article.find({}, function(err, response) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(response);
+        }
+    })
+})
+
+app.listen(port, function() {
+    console.log("app is listening on 8000");
+})
+
+//req = client, res = server
+
+// sendFile = for sending an HTML file
+// sendJSON
+// sendRawText
+//--// express methods
